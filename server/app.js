7966891d189e6d1ff1b4217bfecf73dc7b12d4c8
Federico Spini <federico.spini@gmail.com>
2013-01-14 11:26:47 +0100

    first iteration

--- server/app.js
+++ server/app.js
@@ -20,67 +20,154 @@ var component_json = {
 var express = require('express');
 var shortid = require('shortid');
 var async = require('async');
+var fs = require('fs');
+var extend = require('extend');
+var rimraf = require('rimraf');
+var exec = require('child_process').exec;
 
-var playgrounds = {}
+var playgrounds = {};
 var app = express();
 
 
 /**
  * create_filesystem
+ * create foldes `playgrounds/<id>`.
+ *
+ * @param {Object} options
+ *   @param {String} options.id
+ *   @param {Object} options.data
+ * @api private
  */
 
 var create_filesystem = function(options, callback) {
-  
-  callback(null, options);
+  var id = options.id;
+  var path = 'public/playgrounds/' + id;
+  fs.mkdir(path, function(err) {
+    if (err) throw err;
+    callback(null, options);
+  });
 };
 
 /**
  * create_component_json
+ * create file `playgrounds/<id>/component.json`.
+ *
+ * @param {Object} options
+ *   @param {String} options.id
+ *   @param {Object} options.data
+ * @api private
  */
 
+
 var create_component_json = function(options, callback) {
+  var id = options.id;
+  var data = options.data;
+  var path = 'public/playgrounds/' + id + '/component.json';
+  var component = {};
+  var string;
   
-  callback(null, options);
+  extend(true, component, component_json);
+  component.name += id;
+  component.description += id;
+  component.dependencies = {};
+  data.dependencies.forEach(function(item) {
+    if (item !== '') {
+      component.dependencies[item] = "*";      
+    }
+  });
+  string = JSON.stringify(component);
+  fs.writeFile(path, string, function(err) {
+    if (err) throw err;
+    callback(null, options);
+  });
 };
 
 /**
  * create_makefile
+ * create a symbolic link to `asset/Makefile`.
+ *
+ * @param {Object} options
+ *   @param {String} options.id
+ *   @param {Object} options.data
+ * @api private
  */
 
+
 var create_makefile = function(options, callback) {
-  
-  callback(null, options);
+  var id = options.id;
+  var srcpath = '../../../assets/Makefile';
+  var dstpath = 'public/playgrounds/' + id + '/Makefile';
+  fs.symlink(srcpath, dstpath, 'file', function() {
+    callback(null, options);    
+  });
 };
 
 /**
  * create_html
+ * create file `playgrounds/<id>/index.html`
+ *
+ * @param {Object} options
+ *   @param {String} options.id
+ *   @param {Object} options.data
+ * @api private
  */
 
+
 var create_html = function(options, callback) {
+  var id = options.id;
+  var data = options.data;
+  var path = 'public/playgrounds/' + id + '/index.html';
+  var html = data.html;
   
-  callback(null, options);
+  fs.writeFile(path, html, function(err) {
+    if (err) throw err;
+    callback(null, options);
+  });
 };
 
 /**
  * create_eraser
+ * bind a crone to erase folder `playgrounds/<id>` after 1h.
+ *
+ * @param {Object} options
+ *   @param {String} options.id
+ *   @param {Object} options.data
+ * @api private
  */
 
 var create_eraser = function(options, callback) {
-  
+  var id = options.id;
+  var path = 'public/playgrounds/' + id;
+  playgrounds[id] = setTimeout(function() {
+    rimraf(id, function(err) {
+      if (err) throw err;
+      
+    });
+  }, 3600000);
   callback(null, options);
 };
 
 /**
  * build
+ * build the project.
+ *
+ * @param {Object} options
+ *   @param {String} options.id
+ *   @param {Object} options.data
+ * @api private
  */
 
 var build = function(options, callback) {
+  var id = options.id;
+  var cmd = 'cd public/playgrounds/' + id + '; make'; 
   
-  callback(null, options);
+  console.log('BUILDING...');
+  
+  exec(cmd, function(err) {
+    if (err) throw err;
+    callback(null, options);    
+  });
 };
-
-
-
 
 app
   .use(express.logger('dev'))
@@ -93,6 +180,8 @@ app.post('/build', function(req, res) {
   var id = shortid.generate();
   var redirect_url = '/playgrounds/' + id;
   
+  console.log('building dependencies: ' + req.body.dependencies);
+  
   async.waterfall([
     function(callback) {
       callback(null, {id: id, data: req.body});
@@ -102,12 +191,13 @@ app.post('/build', function(req, res) {
     create_html,
     create_makefile,
     create_eraser,
-    build,
+    build
   ], function (err, result) {
     if (err) {
       console.log(err);
       return;
     }
+    console.log('DONE!');
     res.redirect(redirect_url);
   });
 });
